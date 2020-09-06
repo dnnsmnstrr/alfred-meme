7b23092a62c90893020508f453c6388d399081ec
Dennis Muensterer <dennismuensterer@gmail.com>
2020-09-06 22:10:30 +0200

    Init meme

---
+++ index.js
@@ -0,0 +1,40 @@
+const alfy = require('alfy');
+
+const BASE_URL = 'https://api.memegen.link/'
+const maxAge = 7 * 60 * 60 * 1000 // 1 Week
+const data = await alfy.fetch(BASE_URL + 'templates', {maxAge});
+
+const [input, ...restInput] = alfy.input.split(' ')
+
+const generateUrl = (key) => {
+  return BASE_URL + `images/${key}${restInput.reduce((previous, current) => previous + '/' + current, '')}`
+}
+
+const textReducer = (previous, current, index) => {
+  return `${previous}${current ? (index + 1) + ': ' + current + ', ' : ''}`
+}
+
+const getSubtitle = (name) => {
+  if (!restInput || restInput.length === 0 || restInput[0] === '') return `Add text to generate ${name} meme`
+  return restInput.reduce(textReducer, '').replace('_', ' ')
+}
+
+const items = alfy
+.matches(input, data, 'name')
+.map(({name, blank, source, sample, key}) => ({
+	title: name,
+	subtitle: getSubtitle(name),
+	arg: generateUrl(key),
+  mods: {
+    alt: {
+      arg: source,
+      subtitle: 'Show source',
+    },
+    ctrl: {
+      arg: sample,
+      subtitle: 'Show sample',
+    }
+  }
+}));
+
+alfy.output(items);
---
+++ readme.md
@@ -0,0 +1,22 @@
+# alfred-meme [![Build Status](https://travis-ci.org/dnnsmnstrr/alfred-meme.svg?branch=master)](https://travis-ci.org/dnnsmnstrr/alfred-meme)
+
+> Generate memes using Alfred &amp; memegen.link
+
+
+## Install
+
+```
+$ npm install --global alfred-meme
+```
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `meme`, <kbd>Enter</kbd>, and your query.
+
+
+## License
+
+MIT © [Dennis Muensterer](https://muensterer.xyz)
